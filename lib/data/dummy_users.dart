034f324df55a0666990a215c873683ff80bd07c1
Milena <milenramiro@gmail.com>
2021-01-14 11:07:32 -0300

    Primeiro commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(id: '1',name: 'Maria', email: 'maria@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'),
+  '2': const User(id: '2', name:'Rafael', email: 'rafael@alunos.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png'),
+};
